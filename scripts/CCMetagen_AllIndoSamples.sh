151cc41d1b820b814a2a1608043d9bdb50d39969
Katalina Bobowik <kbobowik@student.unimelb.edu.au>
2020-07-07 16:30:02 +1000

    Update CCMetagen pipeline
    Remove additional parameters, as there are run in another script.

--- scripts/CCMetagen_AllIndoSamples.sh
+++ scripts/CCMetagen_AllIndoSamples.sh
@@ -4,17 +4,12 @@
 source activate py37
 
 # run CCMetagen
-inputdir=/data/cephfs/punim0586/kbobowik/CCmetagen/IndoSample
+inputdir=/data/cephfs/punim0586/kbobowik/EpidemiologicalSurvey/controlSamples_SnakemakeWorkflow/output/50BPControlSamplesCCMetagen_TE
 array_dir=/data/cephfs/punim0586/kbobowik/EpidemiologicalSurvey/scripts/array_files
+noRNA_dir=/data/cephfs/punim0586/kbobowik/EpidemiologicalSurvey/101BPTrimmedIndonesianSamples/output/101BPIndonesianSamplesCCMetagen_TE_noRNA
 
 for sample in ${inputdir}/*.res; do
 	sampleID=$(basename $sample .res)
 	#echo CCMetagen.py -i $sample -o ~/CCMetagenOutput/${sampleID}
 	echo CCMetagen.py -i $sample -o ~/CCMetagenOutput/${sampleID} --depth_unit rpm --mapstat ${sample/.res}.mapstat --depth 1
 done > ${array_dir}/CCMetagen_AllIndoFiles.txt
-
-# Produce summary table
-CCMetagen_merge.py --input_fp ~/CCMetagenOutput/ --keep_or_remove r --filtering_tax_level Phylum --taxa_list Chordata,Arthropoda --output_fp IndoUnmapped_species_table_Helminths_RPM
-
-
-
