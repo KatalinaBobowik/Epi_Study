0e3056d43fdf0bcc294d979a0f411cd91c22652e
Katalina Bobowik <kbobowik@student.unimelb.edu.au>
2020-07-09 21:57:14 +1000

    Added code to remove single hits to just one ref sequence

--- scripts/CCMetagen_rRNARemovalAndSummaryStats.sh
+++ scripts/CCMetagen_rRNARemovalAndSummaryStats.sh
@@ -32,16 +32,36 @@ for folder in ${folders[@]}; do
 	CCMetagen_merge.py --input_fp ${folder}/ --keep_or_remove r --filtering_tax_level Phylum --taxa_list Chordata,Arthropoda --output_fp ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM
 done
 
-# get total number of accession hits and total number of samples for each species
+# get total number of accession hits and total number of samples for each species an order
 IFS=$'\n'
 for folder in ${folders[@]}; do
 	speciesIndex=`head -n1 ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv |tr ',' '\n'|wc -l`
 	for species in `cat ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv| cut -f$speciesIndex -d"," | sort | uniq`; do
-		allInstances=`grep -i $species ${folder}/*_noRNA.csv | cut -f2 -d"." | sort | uniq | printf "\`\echo $species\` \`wc -l\`\n"`
+		allInstances=`grep -i $species ${folder}/*_noRNA.csv | cut -f2 -d"|" | cut -f1 -d" " | sort | uniq | wc -l`
 		totalSamples=`grep -i $species ${folder}/*_noRNA.csv | cut -f1 -d":" | sort | uniq | wc -l`
-		printf "`echo $allInstances` `echo $totalSamples` \n"
+		maxAcessionNumberLength=`grep -i $species ${folder}/*_noRNA.csv | cut -f3 -d"\"" | cut -f4 -d "," | sort -nr | head -n1`
+		printf "`echo $species`\t`echo $allInstances`\t`echo $totalSamples`\t`echo $maxAcessionNumberLength`\t\n"
 	done > ${folder}/species_accessionHits_totalSamples.txt
+	orderIndex=`echo "$(($speciesIndex-3))"`
+	for order in `cat ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv| cut -f$orderIndex -d"," | sort | uniq`; do
+		allInstances=`grep -i $order ${folder}/*_noRNA.csv | cut -f2 -d"|" | cut -f1 -d" " | sort | uniq | wc -l`
+		totalSamples=`grep -i $order ${folder}/*_noRNA.csv | cut -f1 -d":" | sort | uniq | wc -l`
+		maxAcessionNumberLength=`grep -i $family ${folder}/*_noRNA.csv | cut -f3 -d"\"" | cut -f4 -d "," | sort -nr | head -n1`
+		printf "`echo $order`\t`echo $allInstances`\t`echo $totalSamples`\t`echo $maxAcessionNumberLength`\t\n"
+	done > ${folder}/order_accessionHits_totalSamples.txt
 done
 
-
+# Remove pathogens from CCMetagen merged file where reads only map to one accession number and are less than 1000BP
+for folder in ${folders[@]}; do
+	for sample in ${folder}/*_noRNA.csv; do
+		ID=`basename $sample _noRNA.csv`
+		grep -vf <(awk -F "\t" '{if($2==1)print $0}' ${folder}/order_accessionHits_totalSamples.txt | cut -f1) ${folder}/${ID}_noRNA.csv > ${folder}/${ID}_lowAccessionRemoval_order.csv
+	done
+	# remove orginal .csv files
+	find $folder -name '*noRNA.csv' -and -not -name '*lowAccessionRemoval_order.csv' -type f -exec rm '{}' \;
+	# Remove original csv file made by CCMetagen_merge
+	find $folder -name 'ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv' -type f -exec rm '{}' \;
+	# Produce summary table
+	CCMetagen_merge.py --input_fp ${folder}/ --keep_or_remove r --filtering_tax_level Phylum --taxa_list Chordata,Arthropoda --output_fp ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_noLowAccession_RPM
+done
 
