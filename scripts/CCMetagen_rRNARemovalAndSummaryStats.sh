c28f3ce7b150d00dee05c07a7a63c88012600559
Katalina Bobowik <kbobowik@student.unimelb.edu.au>
2020-07-15 10:28:42 +1000

    Add in loop to get accession number for each phylum
    I added in a loop to get the number of hits for each accession number
    for each phylum. Rather than the previous loop, this is stored in one
    file rather than outputting multiple files.

--- scripts/CCMetagen_rRNARemovalAndSummaryStats.sh
+++ scripts/CCMetagen_rRNARemovalAndSummaryStats.sh
@@ -36,20 +36,53 @@ done
 IFS=$'\n'
 for folder in ${folders[@]}; do
 	speciesIndex=`head -n1 ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv |tr ',' '\n'|wc -l`
-	for species in `cat ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv| cut -f$speciesIndex -d"," | sort | uniq`; do
-		allInstances=`grep -i $species ${folder}/*_noRNA.csv | cut -f2 -d"|" | cut -f1 -d" " | sort | uniq | wc -l`
-		totalSamples=`grep -i $species ${folder}/*_noRNA.csv | cut -f1 -d":" | sort | uniq | wc -l`
-		maxAcessionNumberLength=`grep -i $species ${folder}/*_noRNA.csv | cut -f3 -d"\"" | cut -f4 -d "," | sort -nr | head -n1`
-		printf "`echo $species`\t`echo $allInstances`\t`echo $totalSamples`\t`echo $maxAcessionNumberLength`\t\n"
-	done > ${folder}/species_accessionHits_totalSamples.txt
-	orderIndex=`echo "$(($speciesIndex-3))"`
-	for order in `cat ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv| cut -f$orderIndex -d"," | sort | uniq`; do
-		allInstances=`grep -i $order ${folder}/*_noRNA.csv | cut -f2 -d"|" | cut -f1 -d" " | sort | uniq | wc -l`
-		totalSamples=`grep -i $order ${folder}/*_noRNA.csv | cut -f1 -d":" | sort | uniq | wc -l`
-		maxAcessionNumberLength=`grep -i $family ${folder}/*_noRNA.csv | cut -f3 -d"\"" | cut -f4 -d "," | sort -nr | head -n1`
-		printf "`echo $order`\t`echo $allInstances`\t`echo $totalSamples`\t`echo $maxAcessionNumberLength`\t\n"
-	done > ${folder}/order_accessionHits_totalSamples.txt
+	for i in $(seq 0 6); do
+		index=`expr $speciesIndex - $i`
+		for taxonomicRank in `cat ${folder}/ControlUnmapped_species_table_noRepeats_noRNA_RPM.csv| cut -f$index -d"," | sort | uniq`; do
+			allInstances=`grep -i $taxonomicRank ${folder}/*_noRNA.csv | cut -f2 -d"|" | cut -f1 -d" " | sort | uniq | wc -l`
+			totalSamples=`grep -i $taxonomicRank ${folder}/*_noRNA.csv | cut -f1 -d":" | sort | uniq | wc -l`
+			maxAcessionNumberLength=`grep -i $taxonomicRank ${folder}/*_noRNA.csv | cut -f3 -d"\"" | cut -f4 -d "," | sort -nr | head -n1`
+			fullTaxonomicRank=`grep -i $taxonomicRank ${folder}/*_noRNA.csv | awk -F ',' '{i = 8; for (--i; i >= 0; i--){ printf "%s,",$(NF-i)} print ""}' | head -n1`
+			printf "`echo $fullTaxonomicRank`\t`echo $taxonomicRank`\t`echo $allInstances`\t`echo $totalSamples`\t`echo $maxAcessionNumberLength`\t\n"
+		done > ${folder}/${i}_accessionHits_totalSamples.txt
+	done
 done
+
+# do the same thing as abive but comine everything into one file --------
+
+# get only accession number and taconomic ranks
+for folder in ${folders[@]}; do
+	for file in ${folder}/*_noRNA.csv; do
+		noHeaderfile=`tail -n +2 $file`
+		accession=`echo "$noHeaderfile" | cut -f2 -d"|" | cut -f1 -d"."`
+		taxonomicRanks=`echo "$noHeaderfile" | cut -f3 -d"\"" | cut -f13-21 -d","`
+		paste <(echo "$accession") <(echo "$taxonomicRanks") --delimiters '\t'
+	done > ${folder}/accessionAndTaxonomicRanks.csv
+	# get only unique instances of taxonomic ranks
+	cat ${folder}/accessionAndTaxonomicRanks.csv | cut -f2 | sort | uniq > ${folder}/uniqueTaxonomicRanks.txt
+	# filter out chordata and arthropoda
+	grep -v 'Chordata\|Arthropoda' ${folder}/uniqueTaxonomicRanks.txt > ${folder}/uniqueTaxonomicRanks_Filtered.txt
+	while read line; do
+		kingdom=`echo $line | cut -f2 -d","`
+		kingdom_instance=`[[ ! -z "$kingdom" ]] && awk -v pat="$kingdom" -F, '$2==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		phylum=`echo $line | cut -f3 -d","`
+		phylum_instance=`[[ ! -z "$phylum" ]] && awk -v pat="$phylum" -F, '$3==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		class=`echo $line | cut -f4 -d","`
+		class_instance=`[[ ! -z "$class" ]] && awk -v pat="$class" -F, '$4==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		order=`echo $line | cut -f5 -d","`
+		order_instance=`[[ ! -z "$order" ]] && awk -v pat="$order" -F, '$5==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		family=`echo $line | cut -f6 -d","`
+		family_instance=`[[ ! -z "$family" ]] && awk -v pat="$family" -F, '$6==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		genus=`echo $line | cut -f7 -d","`
+		genus_instance=`[[ ! -z "$genus" ]] && awk -v pat="$genus" -F, '$7==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		species=`echo $line | cut -f8 -d","`
+		species_instance=`[[ ! -z "$species" ]] && awk -v pat="$species" -F, '$8==pat' ${folder}/accessionAndTaxonomicRanks.csv | cut -f1 | sort | uniq | wc -l || echo 0`
+		printf "`echo $kingdom`\t`echo $kingdom_instance`\t`echo $phylum`\t`echo $phylum_instance`\t`echo $class`\t`echo $class_instance`\t`echo $order`\t`echo $order_instance`\t`echo $family`\t`echo $family_instance`\t`echo $genus`\t`echo $genus_instance`\t`echo $species`\t`echo $species_instance`\n"
+	done < ${folder}/uniqueTaxonomicRanks_Filtered.txt > ${folder}/summaryAccessionTable.txt
+done
+
+in `cat ${folder}/uniqueTaxonomicRanks_Filtered.txt`; 
+
 
 # Remove pathogens from CCMetagen merged file where reads only map to one accession number and are less than 1000BP
 for folder in ${folders[@]}; do
